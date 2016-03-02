b7136ea2c70f1cad49ac87768984a3050f1d70a7
kajyr <carlo.panzi@gmail.com>
2016-03-02 20:02:24 +0100

    asad

---
+++ index.js
@@ -0,0 +1,30 @@
+var player = require('./player.js');
+
+var hand = [{rank: '2', type: 'D'},{rank: 'K', type: 'S'}];
+
+var table = [ {rank: 'K', type: 'C'}, {rank: '3', type: 'D'}, {rank: '5', type: 'T'} ];
+
+
+var gamestate = {
+    "game": 2,
+    "round": 1,
+    "spinCount": 2,
+    "sb": 2,
+    "pot": 100,
+    "commonCards": table,
+    "players": [{
+    	cards: hand
+    }],
+    // Index of the current player.
+    "me": 0,
+
+    "callAmount": 10
+
+};
+
+
+player.bet(gamestate, function(amount) {
+
+	console.log(amount);
+
+})
